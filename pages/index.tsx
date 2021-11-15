ebea2f151e579703f08e60989dc5cb7675d29aab
dagmawig <dgebreselasse@gmail.com>
2021-11-14 21:53:34 -0600

    added OutsideHours component to Home component to show outside of hours alert when chat button is clicked outside 2-6 pm UTC hours.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,40 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Button, } from "@chakra-ui/react";
 
-const Home: NextPage = () => (
+
+
+const Home: NextPage = () => {
+  function OutsideHours() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>Chat</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>We are Sorry!</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          We are not operating at this time. Please come back at these operating hours: 2pm - 6pm
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+  }
+
+  let d = new Date();
+
+  return (
   <div>
     <Head>
       <title>LineHayat</title>
@@ -14,9 +46,11 @@ const Home: NextPage = () => (
       <h1>
         LineHayat homepage
       </h1>
-      <Link href="/chat">Chat</Link>
+      {(d.getUTCHours()<14 || d.getUTCHours()>=18)? <Link href="/chat">Chat</Link> :
+      <OutsideHours/>}
     </main>
+
   </div>
-);
+)};
 
 export default Home;
