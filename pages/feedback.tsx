4d9f3062a3278adde85d9eede2ac2ed364fcdb9d
Alex Araiza <alexandre.araiza@gmail.com>
2022-01-16 17:52:38 +0100

    Add Chakra UI to feedback page

--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import { ChakraProvider } from '@chakra-ui/react';
+
 import FeedbackForm from '../components/FeedbackForm';
 
 const Feedback: NextPage = () => (
@@ -11,7 +13,9 @@ const Feedback: NextPage = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <FeedbackForm />
+    <ChakraProvider>
+      <FeedbackForm />
+    </ChakraProvider>
   </>
 );
 
