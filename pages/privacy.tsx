4d76224d5edefdd212a4651ffb34f9cc3815af25
Dan Chepkwonoy <dkchepkwony@bsu.edu>
2022-03-02 23:22:08 -0500

    Add privacy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,57 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Privacy.module.css';
+
+const Privacy: NextPage = () => (
+  <>
+    <Head>
+      <title>Privacy - LineHayat</title>
+      <meta name="description" content="LineHayat description" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <div className={styles.main}>
+      <div>
+        <h2>Privacy and Confidentiality Policy</h2>
+        <p>This document states the confidentiality and personal information handling practices of LineHayat (‘us’, ‘we’, or ‘our’).</p>
+      </div>
+      <div>
+        <h2>Overview</h2>
+        <p>We hope to establish a clear understanding with our users, herein referred to as ‘Sharers’ or ‘you’, regarding our confidentiality and privacy handling practices. We hope this policy provides our Sharers with a good understanding and promotes confidence as well as trust in the services provided by LineHayat.</p>
+      </div>
+      <div>
+        <h2>Scope</h2>
+        <p>This policy covers LineHayat’s handling of personal information of our Sharers for Live Chat and Call services. To keep our Sharers’ information safe, we operate by strict guidelines to ensure anonymity and confidentiality. This policy also covers the risks of using our services and limitations of the service we are able to provide. We may update this policy from time to time, and changes will be publicly announced before its implementation.</p>
+      </div>
+      <div>
+        <h2>Anonymity</h2>
+        <p>One of the unique features of the Listening Services provided by LineHayat is its emphasis on anonymity. Both Listening Volunteers and Sharers will remain anonymous at all times. Therefore, our helpline is designed in such a way that our Listening Volunteers will not be able to track the Sharer’s phone number, identity or any other personal information.</p>
+      </div>
+      <div>
+        <h2>Confidentiality</h2>
+        <p>At LineHayat, confidentiality is our utmost priority. Under no circumstance will our Listening Volunteers disclose your information or the content of your sharing with any third parties. All conversations will be automatically deleted when the chat ends. No data will be stored or kept regardless of whether if they were communicated to us through calls or messages.</p>
+      </div>
+      <div>
+        <h2>Changes to this Privacy Statement</h2>
+        <p>Our privacy policy may be revised to refine our Listening Service for the Sharers. An official announcement will be made through our website at least one (1) month before its enforcement, which will also be broadcasted across our social media platforms. If you have any queries, kindly email us at linehayat@usm.my. We will get back to you within 3 days.</p>
+      </div>
+      <div>
+        <h2>How to give feedback, make a complaint, or contact us</h2>
+        <p>We appreciate your support and value your feedback! Please fill in the feedback form on our website, or email us at linehayat@usm.my. We would love to know if you have any complaints, or would like to share your thoughts and comments regarding our services, or have any feedback and suggestions regarding the information presented on this website. We are here for you and hope to keep improving our services.</p>
+      </div>
+      <div>
+        <h2>Risks of using LineHayat</h2>
+        <p>Our Sharers should be aware of certain personal risks of using our services, including but not limited to you accidentally disclosing your personal information such as your institution, identity or those of the people involved in your issue, your location, etc. However, we at LineHayat stand by our principles of anonymity and confidentiality. Your information remains safe with us and will not be shared with others.</p>
+        <p>Some Sharers might also experience distress during their sharing, especially if the issues concerned are heavy or triggering for them. If this happens to you, please let our Listening Volunteers know so that we can try to help you overcome it to the best of our abilities. We would also wish to remind our Sharers that you are free to end the conversation at any point during the communication if you feel it is necessary.</p>
+        <p>During peak hours, our helpline might be frequently engaged due to high rates of call traffic. In this case, we might not be able to pick up every call. We hope you don’t get discouraged and are most welcomed to refer to our list of other similar resources for help.</p>
+      </div>
+      <div>
+        <h2>Disclaimer</h2>
+        <p>LineHayat is a helpline run by trained volunteers. We are trained to provide an anonymous and confidential listening ear to Sharers who need it. The output from our volunteers should not be used as a substitute for professional mental health advice. Due to our limitations, our Listening Volunteers also do not practise crisis intervention, referrals or follow-ups.</p>
+        <p>LineHayat will not be held liable or responsible for any consequences, actions, or incidents upon whoever has used our services. However, if you need any guidance in reaching out for professional mental health help, our Listening Volunteers will be ready to channel you to those resources depending on your individual circumstances.</p>
+      </div>
+    </div>
+  </>
+);
+
+export default Privacy;
